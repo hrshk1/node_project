cbc56e614bfebc1dabc90d0aa86770be89a35eb9
hrshk1 <hrsh.110070391@gmail.com>
2025-05-25 19:04:22 +0530

    Shift mongodb to online server atlas

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db = require('./db');
 const Menu = require('./models/Menu');
 const { error } = require('console');
 app.use(express.json()); //middleware
-
+require('dotenv').config();
 
 //import the router file:
 const menuRoutes = require('./routes/menuroutes');
@@ -14,10 +14,18 @@ app.use('/menu',menuRoutes)
 
 
 
-
-app.listen(3000, () => {
+const PORT = process.env.PORT ||3000
+app.listen(PORT, () => {
     console.log('listening on port 3000')
 })
 //if file is ready to be commited that means it is only added then vs code will show green color
 //if commited, it becomes normal color
 //if committed and chnages are being made then yellow color will appear
+
+//procedure for github:
+//make a new repo
+//write the codes written in intrustion in the terminal 
+//code will be uploaded
+
+//only after git push, changes are reflected in git hub
+//similarly there is git pull to extract changes in github to local
